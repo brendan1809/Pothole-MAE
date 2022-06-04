81ce36d272970605482e42078548c3f20366d21f
Brendan Lim <55783210+brendan1809@users.noreply.github.com>
2022-06-05 01:55:56 +0800

    feat: initial commit

---
+++ app/src/main/java/com/apu/pothole/ui/login/LoginFormState.kt
@@ -0,0 +1,10 @@
+package com.apu.pothole.ui.login
+
+/**
+ * Data validation state of the login form.
+ */
+data class LoginFormState(
+    val usernameError: Int? = null,
+    val passwordError: Int? = null,
+    val isDataValid: Boolean = false
+)
